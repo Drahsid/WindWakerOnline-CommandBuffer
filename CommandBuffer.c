e758caa9ee72e52bfb4b62042fa2b4de3b68a183
Drahsid <Drahsid@gmail.com>
2021-06-03 00:19:03 -0400

    Option for execute code cave, as well as draw for testing

--- CommandBuffer.c
+++ CommandBuffer.c
@@ -8,93 +8,103 @@ extern "C" {
 
 #define OFFSETOF(TYPE, ELEMENT) ((uint32_t)&(((TYPE*)0)->ELEMENT))
 
-extern void dCcS_Move(void* this);
-void CommandBuffer_Update(void*);
+extern void dCcS_Move(void* this); // use for caving draw
+extern void* f_op_overlap_mng__IsPeek(void); // use for caving execute
 
-__attribute__((aligned(0x20))) void CommandBuffer_Update(void* that) {
+uint32_t CommandBuffer_Update(void*);
+
+__attribute__((aligned(0x20))) uint32_t CommandBuffer_Update(void* that) {
     uint32_t index;
     uint32_t qndex;
     uint32_t actorPointer = 0;
     ActorManager_Params* actorParams = 0;
     Command* command;
     CommandReturn* retCommand;
+    uint32_t returnIsPeek = 0;
 
     // Since we are codecaving this function call, we need to actually call it with its original arguments before doing our stuff
+#ifdef USE_DRAW_CODECAVE
     dCcS_Move(that);
+    {
+#else
+    returnIsPeek = f_op_overlap_mng__IsPeek();
+    if (returnIsPeek) {
+#endif
 
-    // iterate through the current active commands on the command buffer
-    for (index = 0; index < cmdBuffer.count; index++) {
-        // shortcut to current command
-        command = &cmdBuffer.commands[index];
+        // iterate through the current active commands on the command buffer
+        for (index = 0; index < cmdBuffer.count; index++) {
+            // shortcut to current command
+            command = &cmdBuffer.commands[index];
 
-        // process this command based on its type
-        switch(command->type) {
-            // if the command is empty, assume there is nothing to do
-            case COMMAND_TYPE_NONE:
-                break;
+            // process this command based on its type
+            switch(command->type) {
+                // if the command is empty, assume there is nothing to do
+                case COMMAND_TYPE_NONE:
+                    break;
 
-            // if the command is puppet spawn
-            case COMMAND_TYPE_PUPPET_SPAWN:
-                // check for free return slots (we need to return the pointer to the puppet, so it is important that we can actually do so)
-                for (qndex = 0; qndex < COMMAND_MAX; qndex++) {
-                    // if the uuid in this slot is 0
-                    if (cmdBuffer.returns[qndex].returnUUID == 0) {
-                        // we will use this slot
-                        retCommand = &cmdBuffer.returns[qndex];
-                        break;
+                // if the command is puppet spawn
+                case COMMAND_TYPE_PUPPET_SPAWN:
+                    // check for free return slots (we need to return the pointer to the puppet, so it is important that we can actually do so)
+                    for (qndex = 0; qndex < COMMAND_MAX; qndex++) {
+                        // if the uuid in this slot is 0
+                        if (cmdBuffer.returns[qndex].returnUUID == 0) {
+                            // we will use this slot
+                            retCommand = &cmdBuffer.returns[qndex];
+                            break;
+                        }
                     }
-                }
 
-                // if free return slot, spawn actor and set the data to the resulting pointer
-                if (qndex != COMMAND_MAX) {
-                    /*
-                        This way sucks, we're doing it manually
-                        // set the return pointer to the result of Actor_Spawn
-                        //cmdBuffer.returns[qndex].data[0] = Actor_Spawn(0xB5, 0, (Vec3f*)0x803E440C, *((uint8_t*)0x803F6A78), 0, 0, 0, 0);
+                    // if free return slot, spawn actor and set the data to the resulting pointer
+                    if (qndex != COMMAND_MAX) {
+                        /*
+                            This way sucks, we're doing it manually
+                            // set the return pointer to the result of Actor_Spawn
+                            //cmdBuffer.returns[qndex].data[0] = Actor_Spawn(0xB5, 0, (Vec3f*)0x803E440C, *((uint8_t*)0x803F6A78), 0, 0, 0, 0);
 
-                        This way also sucks because it doesn't work, still doing it manually
-                        //actorPointer = ActorManager_CreateFast(0xB5, 0, (Vec3f*)0x803E440C, *((uint8_t*)0x803F6A78), 0, 0, 0, 0, 0);
-                    */
+                            This way also sucks because it doesn't work, still doing it manually
+                            //actorPointer = ActorManager_CreateFast(0xB5, 0, (Vec3f*)0x803E440C, *((uint8_t*)0x803F6A78), 0, 0, 0, 0, 0);
+                        */
 
-                    retCommand->returnUUID = command->returnUUID;
-                    retCommand->type = COMMAND_TYPE_PUPPET_SPAWN;
-                    retCommand->data[0] = -1;
-                    retCommand->data[1] = 0xDEADDEAD;
+                        retCommand->returnUUID = command->returnUUID;
+                        retCommand->type = COMMAND_TYPE_PUPPET_SPAWN;
+                        retCommand->data[0] = -1;
+                        retCommand->data[1] = 0xDEADDEAD;
 
-                    actorParams = ActorManager_CreateAppend(0, (Vec3f*)0x803E440C, *((uint8_t*)0x803F6A78), 0, 0, 0, 0xFFFFFFFF);
-                    if (actorParams == 0) {
-                        retCommand->data[2] = 1;
-                    }
-                    else {
-                        actorPointer = f_pc_manager__FastCreate(0xB5, 0, 0, actorParams);
-                        retCommand->data[3] = (uint32_t)actorParams;
-                        if (actorPointer) {
-                            retCommand->data[0] = actorPointer;
-                            retCommand->data[1] = 0xBEEFBEEF;
+                        actorParams = ActorManager_CreateAppend(0, (Vec3f*)0x803E440C, *((uint8_t*)0x803F6A78), 0, 0, 0, 0xFFFFFFFF);
+                        if (actorParams == 0) {
+                            retCommand->data[2] = 1;
                         }
                         else {
-                            retCommand->data[2] = 2;
+                            actorPointer = f_pc_manager__FastCreate(0xB5, 0, 0, actorParams);
+                            retCommand->data[3] = (uint32_t)actorParams;
+                            if (actorPointer) {
+                                retCommand->data[0] = actorPointer;
+                                retCommand->data[1] = 0xBEEFBEEF;
+                            }
+                            else {
+                                retCommand->data[2] = 2;
+                            }
                         }
                     }
-                }
 
-                break;
+                    break;
 
-            // if the command is puppet despawn
-            case COMMAND_TYPE_PUPPET_DESPAWN:
-                // despawn the actor given the command data
-                Actor_Despawn(command->data.puppetDespawn.params.actor);
-                break;
+                // if the command is puppet despawn
+                case COMMAND_TYPE_PUPPET_DESPAWN:
+                    // despawn the actor given the command data
+                    Actor_Despawn(command->data.puppetDespawn.params.actor);
+                    break;
+            }
+
+            // clear the current command so that it doesn't get reprocessed
+            command->type = COMMAND_TYPE_NONE;
         }
 
-        // clear the current command so that it doesn't get reprocessed
-        command->type = COMMAND_TYPE_NONE;
+        // we iterated through all of the commands, assume there are no more commands to process
+        cmdBuffer.count = 0;
     }
 
-    // we iterated through all of the commands, assume there are no more commands to process
-    cmdBuffer.count = 0;
-
-    return;
+    return returnIsPeek;
 }
 
 #ifdef _LANGUAGE_C_PLUS_PLUS
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm *.o *.elf
-powerpc-linux-gnu-gcc-10 -c CommandBuffer.c -g -Og -nostdlib -mbig-endian -mcpu=750 -std=gnu11 -G 0 -o CommandBuffer.elf
+powerpc-linux-gnu-gcc-10 -c CommandBuffer.c -g -Og -nostdlib -mbig-endian -mcpu=750 -std=gnu11 -G 0 -DUSE_DRAW_CODECAVE -o CommandBuffer.elf
 powerpc-linux-gnu-ld -T CommandBuffer.ld -o CommandBuffer.o CommandBuffer.elf
